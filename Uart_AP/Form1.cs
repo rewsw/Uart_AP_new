4501ce7d07bdf694e879372195cdcf4194132562
David <d2366222@gmail.com>
2020-12-28 17:06:11 +0800

    加入專案檔案。

---
+++ Uart_AP/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uart_AP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
